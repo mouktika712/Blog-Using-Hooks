303aa893457a09a9a7997dfb0f6b6a115c231fe0
Mouktika Namde <mouktikanamde712@gmail.com>
2022-12-15 17:03:29 -0800

    Create a Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,49 @@
+import { useState, useEffect } from "react";
+import firestore from '../firebase';
+import {Link} from 'react-router-dom';
+
 function Home() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        firestore
+        .collection('posts')
+        .get()
+        .then((snapshot) => {
+            const posts = snapshot.docs.map((doc) => {
+                return{
+                    id: doc.id,
+                    ...doc.data()
+                };
+            });
+
+            console.log('posts', posts);
+            setPosts(posts);
+        });
+    }, []);
+
     return (
-        <div className="">
-            Home
+        <div className="home">
+            <h1>Tech Blog</h1>
+
+            <div id="blog-by">Mouktika</div>
+            {
+                // you always need to retun from a map() function
+                posts.map((post, index) => {
+                    return (
+                        <div className="post" key={`post-${index}`}>
+                        <Link to={`/post/${post.id}`}>
+                            <h3>{post.title}</h3>
+                        </Link>
+
+                        <p> {post.subtitle} </p>
+                    </div>
+                    )
+                })
+            }
         </div>
-    )
+    );
 }
 
 export default Home;
